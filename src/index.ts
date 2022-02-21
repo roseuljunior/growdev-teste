f7580f1ba306a349f065f05ea7cd3a1a42251a2e
Roseul Junior <roseuljunior@gmail.com>
2022-02-21 20:39:33 -0300

    criado rota padao e readme

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { request } from 'http'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+
+app.get('/', (request: Request, response: Response) => {
+    return response.send('OK')
+})
 
 // GIT -> versionador (repositório, como fazer o código)
 // GITHUB -> plataforma (local, onde fica o código)
